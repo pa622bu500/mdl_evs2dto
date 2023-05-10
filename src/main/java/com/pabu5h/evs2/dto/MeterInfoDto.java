20cddac61db79885b3089b1e42f03dbd70129070
Oliver Lu <pa596bu500@gmail.com>
2023-05-10 06:11:29 +0000

    add PremiseDto

--- src/main/java/com/pabu5h/evs2/dto/MeterInfoDto.java
+++ src/main/java/com/pabu5h/evs2/dto/MeterInfoDto.java
@@ -2,6 +2,7 @@ package com.pabu5h.evs2.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.xt.utils.DateTimeUtil;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,6 +22,8 @@ public class MeterInfoDto {
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("meter_displayname")
     private String meterDisplayname;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private PremiseDto premise;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String address;
     @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -42,10 +45,11 @@ public class MeterInfoDto {
     @JsonProperty("reading_interval")
     private long readingInterval;
 
-    public Map<String, Object> genFiledMap(){
+    public Map<String, Object> toFiledMap(){
         Map<String, Object> map = new HashMap<>();
         map.put("meter_sn", meterSn);
         map.put("meter_displayname", meterDisplayname);
+        map.put("premise", premise);
         map.put("address", address);
         map.put("status", status);
         map.put("rls_status", rlsStatus);
@@ -55,5 +59,30 @@ public class MeterInfoDto {
         map.put("voltage", voltage);
         map.put("reading_interval", readingInterval);
         return map;
+    }
+    public MeterInfoDto fromFieldMap(Map<String, Object> fieldMap){
+        this.meterSn = fieldMap.get("meter_sn")!=null?
+            fieldMap.get("meter_sn").toString():null;
+        this.meterDisplayname = fieldMap.get("meter_displayname") != null?
+            fieldMap.get("meter_displayname").toString():null;
+
+        Map<String, Object> premiseMap = fieldMap.get("premise")!=null?
+            (Map<String, Object>) fieldMap.get("premise"):null;
+        PremiseDto premiseDto = new PremiseDto(fieldMap);
+        this.premise = premiseMap != null? premiseDto:null;
+
+        this.address = (String) fieldMap.get("address") != null?
+            fieldMap.get("address").toString():null;
+        this.status = (String) fieldMap.get("status")!=null?
+            fieldMap.get("status").toString():null;
+        this.rlsStatus = (String) fieldMap.get("rls_status")!=null?
+            fieldMap.get("rls_status").toString():null;
+        this.kwhTimestamp = fieldMap.get("kwh_timestamp")!=null?
+            DateTimeUtil.getLocalDateTime(fieldMap.get("kwh_timestamp").toString()):null;
+        this.kwhReading = (float) fieldMap.get("kwh_reading");
+        this.current = (float) fieldMap.get("current");
+        this.voltage = (float) fieldMap.get("voltage");
+        this.readingInterval = (long) fieldMap.get("reading_interval");
+        return this;
     }
 }
---
+++ src/main/java/com/pabu5h/evs2/dto/PremiseDto.java
@@ -0,0 +1,49 @@
+package com.pabu5h.evs2.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@Setter
+@Builder
+public class PremiseDto {
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String unit;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String level;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String block;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String building;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("postal_code")
+    private String postalCode;
+
+    public PremiseDto(Map<String, Object> fieldMap){
+        fromFieldMap(fieldMap);
+    }
+    public PremiseDto fromFieldMap(Map<String, Object> fieldMap){
+        this.unit = (String) fieldMap.get("unit");
+        this.level = (String) fieldMap.get("level");
+        this.block = (String) fieldMap.get("block");
+        this.building = (String) fieldMap.get("building");
+        this.postalCode = (String) fieldMap.get("postal_code");
+        return this;
+    }
+
+    public Map<String, Object> toFieldMap(){
+        Map<String, Object> map = new HashMap<>();
+        map.put("unit", unit);
+        map.put("level", level);
+        map.put("block", block);
+        map.put("building", building);
+        map.put("postal_code", postalCode);
+        return map;
+    }
+}
