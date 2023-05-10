b07d5ef5069b4d1919d9a27dacea337ae35bfac0
Oliver Lu <pa596bu500@gmail.com>
2023-05-10 04:27:45 +0000

    add reading_interval to MeterInfoDto

--- src/main/java/com/pabu5h/evs2/dto/MeterInfoDto.java
+++ src/main/java/com/pabu5h/evs2/dto/MeterInfoDto.java
@@ -1,5 +1,6 @@
 package com.pabu5h.evs2.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,18 +13,30 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 public class MeterInfoDto {
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("meter_sn")
     private String meterSn;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("meter_displayname")
     private String meterDisplayname;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String address;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String status;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("rls_status")
     private String rlsStatus;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("kwh_timestamp")
     private LocalDateTime kwhTimestamp;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("kwh_reading")
     private float kwhReading;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private float current;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private float voltage;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("reading_interval")
+    private long readingInterval;
 }
