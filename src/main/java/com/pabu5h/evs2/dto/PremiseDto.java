a2afc532a265b7ba56105914536799b5517e90cc
Oliver Lu <pa596bu500@gmail.com>
2023-05-10 09:55:39 +0000

    misc fix

--- src/main/java/com/pabu5h/evs2/dto/PremiseDto.java
+++ src/main/java/com/pabu5h/evs2/dto/PremiseDto.java
@@ -65,7 +65,9 @@ public class PremiseDto {
         if (postalCode != null) {
             sb.append(postalCode);
         }else {
-            sb.deleteCharAt(sb.length() - 1);
+            if(sb.length() > 0) {
+                sb.deleteCharAt(sb.length() - 1);
+            }
         }
         return sb.toString();
     }
