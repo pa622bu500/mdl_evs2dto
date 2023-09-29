40a9449df93570dda6cde1e0a4f97808abcc002d
pa596 <oliver.lu@pa.com.sg>
2023-09-29 11:18:41 +0800

    add toString ot M3ResponseDto

--- src/main/java/com/pabu5h/evs2/dto/M3ResponseDto.java
+++ src/main/java/com/pabu5h/evs2/dto/M3ResponseDto.java
@@ -33,4 +33,17 @@ public class M3ResponseDto<T> {
     @JsonInclude(JsonInclude.Include.NON_NULL)
 //    @JsonProperty("rls_mid")
     String rlsMid;
+
+    @Override
+    public String toString() {
+        return "M3ResponseDto{" +
+                "success=" + success + ", " +
+                "message='" + message + "', " +
+                "error='" + error + "', " +
+                "status='" + status + "', " +
+                "result='" + result + "', " +
+                "mid='" + mid + "', " +
+                "rls='" + rls + "', " +
+                "rlsMid='" + rlsMid + "'}";
+    }
 }
