68a3bb8dfed83184178266782eccfdc27f7a630a
pa596 <oliver.lu@pa.com.sg>
2023-11-06 11:02:10 +0800

    add first/last data to meta

--- src/main/java/com/pabu5h/evs2/dto/IotHistoryMetaDto.java
+++ src/main/java/com/pabu5h/evs2/dto/IotHistoryMetaDto.java
@@ -22,29 +22,41 @@ public class IotHistoryMetaDto {
     long duration;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("max_val")
-    double maxVal;
+    Double maxVal;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("max_val_dt")
     LocalDateTime maxValDt;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("min_val")
-    double minVal;
+    Double minVal;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("min_val_non_zero")
-    double minValNonZero;
+    Double minValNonZero;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("min_val_non_zero_dt")
     LocalDateTime minValNonZeroDt;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("avg_val")
-    double avgVal;
+    Double avgVal;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("median_val")
-    double medianVal;
+    Double medianVal;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("total")
-    double total;
+    Double total;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("positive_count")
     long positiveCount;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("first_val")
+    Double firstVal;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("first_val_dt")
+    LocalDateTime firstValDt;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("last_val")
+    Double lastVal;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("last_val_dt")
+    LocalDateTime lastValDt;
 }
