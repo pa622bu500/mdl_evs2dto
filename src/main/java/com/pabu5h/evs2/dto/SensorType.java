9467ae6f8fc411a6ff77367e06b727f379ae00e5
ggbotbox <oliver.lu@pa.com.sg>
2023-11-12 00:32:40 +0800

    change enum value to lowerCamel

--- src/main/java/com/pabu5h/evs2/dto/SensorType.java
+++ src/main/java/com/pabu5h/evs2/dto/SensorType.java
@@ -1,9 +1,9 @@
 package com.pabu5h.evs2.dto;
 
 public enum SensorType {
-    SMOKE,
-    TEMPERATURE,
-    HUMIDITY,
-    IR,
-    WATER_LEAK,
+    smoke,
+    temperature,
+    humidity,
+    ir,
+    waterLeak,
 }
