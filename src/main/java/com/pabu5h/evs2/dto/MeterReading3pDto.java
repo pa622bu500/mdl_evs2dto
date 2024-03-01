2fce082db191b7df4ee9e9ee84049691983c799a
pa596 <oliver.lu@pa.com.sg>
2024-03-01 13:10:32 +0800

    use Double in MeterReading3pDto

--- src/main/java/com/pabu5h/evs2/dto/MeterReading3pDto.java
+++ src/main/java/com/pabu5h/evs2/dto/MeterReading3pDto.java
@@ -32,60 +32,60 @@ public class MeterReading3pDto {
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("ptp_v_l1")
-    private double ptpVoltageL1;
+    private Double ptpVoltageL1;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("ptp_v_l2")
-    private double ptpVoltageL2;
+    private Double ptpVoltageL2;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("ptp_v_l3")
-    private double ptpVoltageL3;
+    private Double ptpVoltageL3;
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("l_c_l1")
-    private double lineCurrentL1;
+    private Double lineCurrentL1;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("l_c_l2")
-    private double lineCurrentL2;
+    private Double lineCurrentL2;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("l_c_l3")
-    private double lineCurrentL3;
+    private Double lineCurrentL3;
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("ap_pf")
-    private double allPhasePowerFactor;
+    private Double allPhasePowerFactor;
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("freq")
-    private double frequency;
+    private Double frequency;
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("ap_a_p_t")
-    private double allPhaseActivePowerTotal;
+    private Double allPhaseActivePowerTotal;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("ap_re_p")
-    private double allPhaseReactivePower;
+    private Double allPhaseReactivePower;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("ap_a_p")
-    private double allPhaseApparentPower;
+    private Double allPhaseApparentPower;
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("a_imp")
-    private double activeImport;
+    private Double activeImport;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("a_exp")
-    private double activeExport;
+    private Double activeExport;
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("re_lag")
-    private double reactiveLag;
+    private Double reactiveLag;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("re_lead")
-    private double reactiveLead;
+    private Double reactiveLead;
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("c_md_sb_a_imp")
-    private double currentMaxDemandSinceBillingActiveImport;
+    private Double currentMaxDemandSinceBillingActiveImport;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("c_md_sb_a_exp")
-    private double currentMaxDemandSinceBillingActiveExport;
+    private Double currentMaxDemandSinceBillingActiveExport;
 }
