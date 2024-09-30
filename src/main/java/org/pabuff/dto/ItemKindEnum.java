7bab2d8f41e109c8498f29f405f82d2be50daeab
pa596 <oliver.lu@pa.com.sg>
2024-09-30 12:29:46 +0800

    add ItemKindEnum

---
+++ src/main/java/org/pabuff/dto/ItemKindEnum.java
@@ -0,0 +1,19 @@
+package org.pabuff.dto;
+
+public enum ItemKindEnum {
+    METER,
+    SENSOR,
+    GATEWAY,
+    TENANT,
+    USER,
+    ROLE,
+    METER_GROUP,
+    CONCENTRATOR,
+    CONCENTRATOR_TARIFF,
+    TARIFF_PACKAGE,
+    TARIFF_PACKAGE_RATE,
+    JOB_TYPE,
+    JOB,
+    JOB_TYPE_SUB,
+    BILLING_REC,
+}
