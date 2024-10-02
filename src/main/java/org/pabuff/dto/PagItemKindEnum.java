b242a21e3a8002a6364c569af0f7c0c3a438dbe0
pa596bu500 <pa596bu500@gmail.com>
2024-10-02 23:29:09 +0800

    add PagItemKindEnum

---
+++ src/main/java/org/pabuff/dto/PagItemKindEnum.java
@@ -0,0 +1,23 @@
+package org.pabuff.dto;
+
+public enum PagItemKindEnum {
+    METER,
+    SENSOR,
+    CAMERA,
+    LOCK,
+    GATEWAY,
+    LOCATION,
+    SITE,
+    TENANT,
+    USER,
+    ROLE,
+    METER_GROUP,
+    CONCENTRATOR,
+    CONCENTRATOR_TARIFF,
+    TARIFF_PACKAGE,
+    TARIFF_PACKAGE_RATE,
+    JOB_TYPE,
+    JOB,
+    JOB_TYPE_SUB,
+    BILLING_REC,
+}
