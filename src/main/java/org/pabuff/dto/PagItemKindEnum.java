255693e2cef5f9b2500f975604dea1496a979f3b
pa596bu500 <pa596bu500@gmail.com>
2024-12-12 00:08:45 +0800

    item kind update

--- src/main/java/org/pabuff/dto/PagItemKindEnum.java
+++ src/main/java/org/pabuff/dto/PagItemKindEnum.java
@@ -2,14 +2,18 @@ package org.pabuff.dto;
 
 public enum PagItemKindEnum {
     DEVICE,
+    DEVICE_GROUP,
+
     LOCATION,
-    SITE,
+    LOCATION_GROUP,
     BUILDING,
+    SITE,
+    SITE_GROUP,
+
     USER,
     ROLE,
     TENANT,
 
-    METER_GROUP,
     CONCENTRATOR,
     CONCENTRATOR_TARIFF,
     TARIFF_PACKAGE,
