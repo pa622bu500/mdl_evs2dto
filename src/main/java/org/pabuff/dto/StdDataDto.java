a3ec182a0b5bee9a83d7ba41810ae7ed1fd3935c
pa596 <oliver.lu@pa.com.sg>
2025-05-05 13:05:41 +0800

    misc update

--- src/main/java/org/pabuff/dto/StdDataDto.java
+++ src/main/java/org/pabuff/dto/StdDataDto.java
@@ -14,6 +14,25 @@ public class StdDataDto {
     @JsonProperty("result_key")
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String resultKey;
+
+    /*
+    result format:
+    {
+        result_key:
+        {
+            ...
+        }
+    }
+    */
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private Map<String, Object> result;
+
+    public Map<String, Object> getResultMap () {
+        Object obj = result.get(resultKey);
+        if (obj instanceof Map) {
+            return (Map<String, Object>) obj;
+        } else {
+            return null;
+        }
+    }
 }
