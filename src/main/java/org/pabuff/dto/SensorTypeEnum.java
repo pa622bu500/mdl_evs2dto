8b5604d22e54fa6e66f7566a59cbb1cd9e6404b3
pabuff-deploy <pabuff.deploy@gmail.com>
2025-08-19 15:38:36 +0800

    recover necessary sensor type

--- src/main/java/org/pabuff/dto/SensorTypeEnum.java
+++ src/main/java/org/pabuff/dto/SensorTypeEnum.java
@@ -2,8 +2,12 @@ package org.pabuff.dto;
 
 public enum SensorTypeEnum {
     SMOKE,
+    TEMPERATURE,
+    HUMIDITY,
     IR,
     WATER_LEAK,
+    METER,
+    METER_3P,
     CO2,
     SWITCH,
     TEMPERATURE_HUMIDITY,
